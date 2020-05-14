7cff0b278c37f9ac52b274ca075d00e4c76929b5
aparappi <aparappi@iu.edu>
2020-05-14 15:59:35 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Coronavirus Stats by US County - Text Message sender
+Sends text message of total confirmed cases, total deaths, new deaths & new cases for counties in the US
+
+# Requirements
+- Python 3.x+
+- Gmail account
+- Receiver phone number in the US (network should be one among AT&T, T-Mobile, Verizon, Sprint or Cricket (or their MVNOs))
+
+# How-to
+- Fill the details in ```msg_info.json```
+	- ```sendTo```: Which phone number do you want to send the SMS to?
+	- ```network```: What is that number's network provider? Enter one of the following: ATT, TMobile, Verizon, Sprint or Cricket (Enter exactly as given. Case sensitive)
+	- You can keep as many entries as you want, the value of X in key ```msgX``` does not matter as long as it is unique
+- In ```sender_info```, write your gmail address in the first line & gmail account password in the second line
+	- **WARNING**: There is no encryption or other means of security here. Storing plain text passwords is very risky and not recommended. This file's security is your responsibility so don't store it somewhere other's can access. This goes for the mobile number stored in ```msg_info.json``` as well. Since this was for educational purposes, I have kept it as-is. Feel free to raise a PR if you wish to contribute towards a safer approach to storing credentials.
+- Run using ```python3 script.py``` or ```python script.py``` depending on your environment setup
---
+++ script.py
@@ -0,0 +1,52 @@
+import requests
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import json
+
+carrier_gateways = {
+	"ATT": "@txt.att.net",
+	"TMobile": "@tmomail.net",
+	"Verizon": "@vtext.com",
+	"Sprint": "@messaging.sprintpcs.com",
+	"Cricket" : "@mms.cricketwireless.net"
+}
+
+# Connecting to GMail SMTP Server
+f = open("sender_info", "r")
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+gmail_address = f.readline()
+gmail_password = f.readline()
+server.login(gmail_address, gmail_password)
+
+with open("msg_info.json", "r") as input_file:
+	all_input = json.load(input_file)
+
+	for v in all_input.values():
+		to_number = v["sendTo"]
+		state = v["state"]
+		county = v["county"]
+		cell_network = v["network"]
+		data = '{"state":\"'+state+'\","county":\"'+county+'\"}'
+
+		# Hitting the API to get stats
+		response = requests.post('https://covid19-us-api.herokuapp.com/county', data=data)
+		m = response.json()['message'][0]
+		confirmed = str(m['confirmed'])
+		deaths = str(m['death'])
+		new = str(m['new'])
+		new_death = str(m['new_death'])
+
+		# Forming SMS body
+		body = "-------------\nConfirmed: "+confirmed+"\nDeaths: "+deaths+"\nNew Cases: "+new + "\nNew Deaths: "+ new_death+"\n----------------"
+
+		msg = MIMEMultipart()
+		msg['Subject'] = "COVID-19 at "+county+" County, "+state
+		msg.attach(MIMEText(body, 'plain'))
+		sms = msg.as_string()
+
+		to = to_number + carrier_gateways[cell_network]
+		server.sendmail(gmail_address, to, sms)
+
+server.close()
